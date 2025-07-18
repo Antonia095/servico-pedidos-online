c2ae469abde2cde0b7f036e5cdd62fb40c5cfdfc
Antonia Maciel <antonia.josy95@gmail.com>
2025-07-17 22:29:57 -0300

    reestrutura projeto adicionando cada servico em modulos diferentes

--- src/main/java/br/com/estudo/pedidos/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.estudo.pedidos.repository;
-
-import br.com.estudo.pedidos.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
-}
